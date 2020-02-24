367388b9579d4ea9a19c5881cc4975ce98dcc22c
carter3689 <carter3689@gmail.com>
2020-02-24 11:10:46 -0600

    First commit of Feb 2020 Cohort JC

---
+++ prework.py
@@ -0,0 +1,54 @@
+# Prework Question 1
+def display_name(name):
+    print(name)
+
+
+
+# Prework Question 2 -- Print 100 Odd Numbers
+def print_odd():
+    for num in range(1, 101):
+        if num % 2 != 0:
+            print(num)
+
+# Prework Question 3 - Find max number in a list
+
+def max_num_in_list(a_list):
+    max = a_list[0]
+    for a in a_list:
+        if a > max:
+            max = a
+            return max
+
+
+# Prework Question 3.a
+def max_num(a_list):
+    return max(a_list)
+
+
+# Prework Question 4 - Function that returns leap year for given year
+def is_leap(year):
+    if year % 4 == 0 and (year % 400 == 0 or year % 100 != 0):
+        print('True')
+    else:
+        print('False')
+
+#  Prework Question 5 -- Check if numbers are consecutive in a list
+
+def is_conscecutive(a_list):
+    total = 2
+    while total > 1:
+        test = a_list.pop(0)
+        if test == a_list[0] - 1:
+            total = len(a_list)
+        else:
+            return False
+    return True
+
+def isconsec(a_list):
+    for i in range(1, len(a_list)):
+        if a_list[i] - 1!= a_list[i -1]:  
+            return False
+    return True
+
+    # [4,5,6,7]
+    
